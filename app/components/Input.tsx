1fac4b25f573b085031fbfd0e864d367072b120a
Peter Hajdu <hajdu2024@gmail.com>
2025-06-18 07:59:27 +0200

    changed some text

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -2,8 +2,9 @@ function Input() {
   return (
     <div className=" flex border-border border-1 rounded-md bg-foreground p-5 flex-col ">
       <form className="flex flex-col gap-5 font-mono ">
-        <input placeholder="From stop" className="py-1 placeholder:text-[#424242] rounded-md text-3xl px-2 bg-inputs" />
-        <input placeholder="To stop" className=" py-1  placeholder:text-[#424242] text-3xl rounded-md px-2 bg-inputs" />
+        <input placeholder="From stop" className="py-1 outline-none placeholder:text-[#424242] rounded-md text-3xl px-2 text-white bg-inputs" />
+
+        <input placeholder="To stop" className=" py-1  placeholder:text-[#424242] text-3xl rounded-md px-2 bg-inputs text-white outline-none" />
         <button className="bg-[#e5e5e5] font-bold rounded-md  py-2 px-5 text-[#171717] ">Search</button>
 
       </form>
