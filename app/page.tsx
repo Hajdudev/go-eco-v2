f64aee8a7775d3847ff184e24f34963641e16bfa
Peter Hajdu <hajdu2024@gmail.com>
2025-06-18 07:14:07 +0200

    input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import Input from "./components/Input";
 
 export default function Home() {
   return (
-  <div></div>
+    <div className="flex w-screen h-screen">
+      <div className="w-1/2 flex justify-center items-center  h-full">
+        <Input />
+      </div>
+      <div className="w-1/2 h-full"></div>
+    </div>
   );
 }
